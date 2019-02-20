352e7030ee5b57c6a14749887caa91846c59c489
chavacava <salvadorcavadini+github@gmail.com>
2019-02-20 09:33:21 +0100

    check skips stdlib packages

--- internal/checker.go
+++ internal/checker.go
@@ -1,25 +1,30 @@
 package internal
 
 import (
-	"github.com/KyleBanks/depth"
 	"errors"
 	"fmt"
 	"strings"
+
+	"github.com/KyleBanks/depth"
 )
 
-type checker struct {
+// Checker models a dependencies constraints checker
+type Checker struct {
 	constraints []plainConstraint
 }
 
-func NewChecker(c []plainConstraint) checker {
-	return checker{constraints: c}
+// NewChecker yields a dependencies constraint checker
+func NewChecker(c []plainConstraint) Checker {
+	return Checker{constraints: c}
 }
 
-func (c checker) String() string {
+// String yields a string representation of this checker
+func (c Checker) String() string {
 	return fmt.Sprintf("Checker constraints: %+v", c.constraints)
 }
 
-func (c checker) CheckPkg(pkg string) (warns []error, errs []error) {
+// CheckPkg checks if the given package respects the dependency constraints of this checker
+func (c Checker) CheckPkg(pkg string) (warns []error, errs []error) {
 	errs = []error{}
 	warns = []error{}
 
@@ -29,58 +34,63 @@ func (c checker) CheckPkg(pkg string) (warns []error, errs []error) {
 		return warns, errs
 	}
 
-	t := depth.Tree {
+	t := depth.Tree{
 		ResolveInternal: true,
-		ResolveTest: true,
+		ResolveTest:     true,
 	}
 
 	err := t.Resolve(pkg)
 	if err != nil {
-		return warns, append(errs,errors.New(fmt.Sprintf("[%s] Unable to get dependencies of %s: %v",Error,pkg,err)))
+		return warns, append(errs, fmt.Errorf("[%s] Unable to get dependencies of %s: %v", Error, pkg, err))
 	}
 
 	pkgDeps := t.Root.Deps
 
 	for _, constr := range applicableConstraints {
-		switch kind:=constr.kind; kind {
+		switch kind := constr.kind; kind {
 		case Allow, Forbid:
-			w, e := checkConstraint(constr,pkg,pkgDeps) 
+			w, e := checkConstraint(constr, pkg, pkgDeps)
 			warns = append(warns, w...)
 			errs = append(errs, e...)
 		default:
-			errs = append(errs,errors.New(fmt.Sprintf("[%s] Unable to check constraints of kind `%s`",Error,kind)))
+			errs = append(errs, fmt.Errorf("[%s] Unable to check constraints of kind `%s`", Error, kind))
 		}
 	}
 
 	return warns, errs
 }
 
-func checkConstraint(c plainConstraint, pkg string, pkgDeps []depth.Pkg) (warns []error,errs []error) {
+func checkConstraint(c plainConstraint, pkg string, pkgDeps []depth.Pkg) (warns []error, errs []error) {
 	errs = []error{}
 	warns = []error{}
-	
+
 	for _, d := range pkgDeps {
+		if d.Internal {
+			continue // skip stdlib packages
+		}
+
 		ok := false
-		for _,t:= range c.targetPatterns {
-			matches := strings.Contains(d.Name,t)
+		for _, t := range c.targetPatterns {
+			matches := strings.Contains(d.Name, t)
 			if matches && c.kind == Allow {
 				ok = true
 				break
 			}
 			if matches && c.kind == Forbid {
+				ok = false
 				break
 			}
 		}
 
 		if !ok {
-			warns, errs = appendByLevel(warns, errs, c.onBreak, fmt.Sprintf("[%s] %s depends on %s",c.onBreak,pkg,d.Name))
+			warns, errs = appendByLevel(warns, errs, c.onBreak, fmt.Sprintf("[%s] %s depends on %s", c.onBreak, pkg, d.Name))
 		}
 	}
 
 	return warns, errs
 }
 
-func appendByLevel(w, e []error, level errorLevel, msg string) (warns []error,errs []error) {
+func appendByLevel(w, e []error, level errorLevel, msg string) (warns []error, errs []error) {
 	newErr := errors.New(msg)
 
 	if level == Warn {
@@ -90,12 +100,12 @@ func appendByLevel(w, e []error, level errorLevel, msg string) (warns []error,er
 	return w, append(e, newErr)
 }
 
-func (c checker) getApplicableConstraints(pkg string) (constraints []plainConstraint) {
+func (c Checker) getApplicableConstraints(pkg string) (constraints []plainConstraint) {
 	constraints = []plainConstraint{}
 	for _, constr := range c.constraints {
 		for _, mp := range constr.modulePatterns {
-			println(">>> check if",pkg,"contains", mp)
-			if strings.Contains(pkg,mp) {
+			println(">>> check if", pkg, "contains", mp)
+			if strings.Contains(pkg, mp) {
 				println(">>> yes")
 				constraints = append(constraints, constr)
 				break
